a7da4aee88aeff54b2c2d245bd033c038cd2768d
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-03 12:53:37 +0100

    updaate record and zones

--- pkg/model/record.go
+++ pkg/model/record.go
@@ -1,18 +1,38 @@
 package model
 
 import (
+	"fmt"
+	"github.com/DataDog/jsonapi"
+	"github.com/oklog/ulid/v2"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Record struct {
-	ID        string         `gorm:"primarykey" jsonapi:"primary,record"`
+	ID        string         `gorm:"primarykey,not null" jsonapi:"primary,records"`
 	CreatedAt time.Time      `jsonapi:"attribute" json:"created_at"`
 	UpdatedAt time.Time      `jsonapi:"attribute" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" jsonapi:"attribute" json:"deleted_at"`
-	Name      string         `gorm:"uniqueIndex" jsonapi:"attribute" json:"name"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Name      string         `gorm:"uniqueIndex,not null" jsonapi:"attribute" json:"name"`
 	TTL       int            `gorm:"default:3600" jsonapi:"attribute" json:"ttl"`
-	Type      string         `jsonapi:"attribute" json:"type"`
-	Data      string         `jsonapi:"attribute" json:"data"`
-	ZoneID    string         `gorm:"foreignKey:ZoneID" jsonapi:"relationship" json:"zone"`
+	Type      string         `jsonapi:"attribute,not null" json:"type"`
+	Data      string         `jsonapi:"attribute,not null" json:"data"`
+	ZoneID    string         `gorm:"foreignKey:ZoneID" jsonapi:"relationship" json:"zone,omitempty"`
+}
+
+func (r *Record) LinkRelation(relation string) *jsonapi.Link {
+	return &jsonapi.Link{
+		Self:    fmt.Sprintf("%s/v1/records/%s/relationships/%s", viper.GetString("serviceUrl"), r.ID, relation),
+		Related: fmt.Sprintf("%s/v1/records/%s/%s", viper.GetString("serviceUrl"), r.ID, relation),
+	}
+}
+
+func (r *Record) BeforeCreate(tx *gorm.DB) (err error) {
+	if r.ID == "" {
+		r.ID = ulid.Make().String()
+	} else {
+		_, err = ulid.Parse(r.ID)
+	}
+	return err
 }
--- pkg/model/zone.go
+++ pkg/model/zone.go
@@ -1,24 +1,44 @@
 package model
 
 import (
+	"fmt"
+	"github.com/DataDog/jsonapi"
+	"github.com/oklog/ulid/v2"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Zone struct {
-	ID        string         `gorm:"primarykey" jsonapi:"primary,record"`
+	ID        string         `gorm:"primarykey,not null" jsonapi:"primary,zones"`
 	CreatedAt time.Time      `jsonapi:"attribute" json:"created_at"`
 	UpdatedAt time.Time      `jsonapi:"attribute" json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" jsonapi:"attribute" json:"deleted_at"`
-	Name      string         `gorm:"uniqueIndex" jsonapi:"attribute" json:"name"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Name      string         `gorm:"uniqueIndex,not null" jsonapi:"attribute" json:"name"`
 	TTL       int            `gorm:"default:3600" jsonapi:"attribute" json:"ttl"`
-	MName     string         `gorm:"default:@" jsonapi:"attribute" json:"mname"`
-	RName     string         `gorm:"default:admin" jsonapi:"attribute" json:"rname"`
+	MName     string         `gorm:"default:@,not null" jsonapi:"attribute" json:"mname"`
+	RName     string         `gorm:"default:admin,not null" jsonapi:"attribute" json:"rname"`
 	Serial    int            `gorm:"default:1" jsonapi:"attribute" json:"serial"`
 	Refresh   int            `gorm:"default:3600" jsonapi:"attribute" json:"refresh"`
 	Retry     int            `gorm:"default:600" jsonapi:"attribute" json:"retry"`
 	Expire    int            `gorm:"default:604800" jsonapi:"attribute" json:"expire"`
 	Minimum   int            `gorm:"default:3600" jsonapi:"attribute" json:"minimum"`
-	Records   []*Record      `gorm:"foreignKey:ZoneID" jsonapi:"relationship" json:"records"`
-	Backends  []*Backend     `gorm:"many2many:backend_zones;" jsonapi:"relationship" json:"backends"`
+	Records   []*Record      `gorm:"foreignKey:ZoneID" jsonapi:"relationship" json:"records,omitempty"`
+	Backends  []*Backend     `gorm:"many2many:backend_zones;" jsonapi:"relationship" json:"backends,omitempty"`
+}
+
+func (z *Zone) LinkRelation(relation string) *jsonapi.Link {
+	return &jsonapi.Link{
+		Self:    fmt.Sprintf("%s/v1/zones/%s/relationships/%s", viper.GetString("serviceUrl"), z.ID, relation),
+		Related: fmt.Sprintf("%s/v1/zones/%s/%s", viper.GetString("serviceUrl"), z.ID, relation),
+	}
+}
+
+func (z *Zone) BeforeCreate(tx *gorm.DB) (err error) {
+	if z.ID == "" {
+		z.ID = ulid.Make().String()
+	} else {
+		_, err = ulid.Parse(z.ID)
+	}
+	return err
 }
