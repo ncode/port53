9c6f21bd741ac4d0a4220836c29f0c96216e3e4d
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-04 22:37:46 +0100

    adds urls for relationship updates and start formating to match jsonapi spec

--- internal/api/backend.go
+++ internal/api/backend.go
@@ -92,10 +92,95 @@ func (r *BackendRoute) Delete(c echo.Context) (err error) {
 	return c.NoContent(http.StatusNoContent)
 }
 
+func (r *BackendRoute) GetZones(c echo.Context) (err error) {
+	var zones []model.Zone
+	r.db.Model(&zones).Where("backend_id = ?", c.Param("id")).Preload("Backend").Find(&zones)
+	marshal, err := jsonapi.Marshal(zones)
+	if err != nil {
+		return err
+	}
+	return c.Blob(http.StatusOK, binder.MIMEApplicationJSONApi, marshal)
+}
+
+func (r *BackendRoute) AddZone(c echo.Context) (err error) {
+	var backend model.Backend
+	r.db.Preload("Zones").First(&backend, "id = ?", c.Param("id"))
+	if backend.ID == "" {
+		return c.String(http.StatusNotFound, "Backend not found")
+	}
+	var zone model.Zone
+	if err := c.Bind(&zone); err != nil {
+		return err
+	}
+	if zone.ID == "" {
+		return c.String(http.StatusBadRequest, "Zone ID is required")
+	}
+	backend.Zones = append(backend.Zones, &zone)
+	err = r.db.Model(&backend).Association("Zones").Append(zone)
+	if err != nil {
+		return err
+	}
+	marshal, err := jsonapi.Marshal(backend)
+	if err != nil {
+		return err
+	}
+	return c.Blob(http.StatusOK, binder.MIMEApplicationJSONApi, marshal)
+}
+
+func (r *BackendRoute) RemoveZone(c echo.Context) (err error) {
+	var backend model.Backend
+	r.db.Preload("Zones").First(&backend, "id = ?", c.Param("id"))
+	if backend.ID == "" {
+		return c.String(http.StatusNotFound, "Backend not found")
+	}
+	var zone model.Zone
+	if err := c.Bind(&zone); err != nil {
+		return err
+	}
+	if zone.ID == "" {
+		return c.String(http.StatusBadRequest, "Zone ID is required")
+	}
+	err = r.db.Model(&backend).Association("Zones").Delete(zone)
+	if err != nil {
+		return err
+	}
+	marshal, err := jsonapi.Marshal(backend)
+	if err != nil {
+		return err
+	}
+	return c.Blob(http.StatusOK, binder.MIMEApplicationJSONApi, marshal)
+}
+
+func (r *BackendRoute) UpdateZone(c echo.Context) (err error) {
+	var backend model.Backend
+	r.db.Preload("Zones").First(&backend, "id = ?", c.Param("id"))
+	if backend.ID == "" {
+		return c.String(http.StatusNotFound, "Backend not found")
+	}
+	var zones []model.Zone
+	if err := c.Bind(&zones); err != nil {
+		return err
+	}
+	if len(zones) == 0 {
+		return c.String(http.StatusBadRequest, "Zone ID is required")
+	}
+	err = r.db.Model(&backend).Association("Zones").Replace(zones)
+	if err != nil {
+		return err
+	}
+	return r.Get(c)
+}
+
 func (r *BackendRoute) Register(e *echo.Echo) {
 	e.GET("/v1/backends/:id", r.Get)
 	e.DELETE("/v1/backends/:id", r.Delete)
 	e.POST("/v1/backends", r.Create)
-	e.PUT("/v1/backends/:id", r.Update)
+	e.PATCH("/v1/backends/:id", r.Update)
 	e.GET("/v1/backends", r.List)
+	// Relationships
+	e.GET("/v1/backends/:id/zones", r.GetZones)
+	e.POST("/v1/backends/:id/zones", r.AddZone)
+	e.PATCH("/v1/backends/:id/zones", r.UpdateZone)
+	e.DELETE("/v1/backends/:id/zones", r.RemoveZone)
+
 }
