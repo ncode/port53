7b89cd71e8169f85678c1e38dff398237bcdf664
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-09 09:49:35 +0100

    moving test to test tuples

--- internal/api/backend_test.go
+++ internal/api/backend_test.go
@@ -29,6 +29,38 @@ var (
 )
 
 func TestCreateBackend(t *testing.T) {
+	tests := []struct {
+		name                   string
+		input                  string
+		expectedData           *model.Backend
+		expectedLocationHeader string
+		expected               int
+	}{
+		{
+			name:         "valid input",
+			input:        `{"data": {"id":"01F1ZQZJXQXZJXZJXZJXZJXZJX", "type": "backends", "attributes": {"name": "bind"}}}`,
+			expectedData: &model.Backend{ID: "01F1ZQZJXQXZJXZJXZJXZJXZJX", Name: "bind"},
+			expected:     http.StatusCreated,
+		},
+		{
+			name:     "invalid input",
+			input:    `{"data": {"type": "backends", "attributes": {"name": ""}}}`,
+			expected: http.StatusBadRequest,
+		},
+		{
+			name:                   "id conflict",
+			input:                  `{"data": {"id":"01F1ZQZJXQXZJXZJXZJXZJXZJX", "type": "backends", "attributes": {"name": "pdns"}}}`,
+			expectedLocationHeader: fmt.Sprintf("%s/v1/backends/%s", viper.GetString("serviceUrl"), backendResult.ID),
+			expected:               http.StatusConflict,
+		},
+		{
+			name:                   "name conflict",
+			input:                  `{"data": {"type": "backends", "attributes": {"name": "bind"}}}`,
+			expectedLocationHeader: fmt.Sprintf("%s/v1/backends/%s", viper.GetString("serviceUrl"), backendResult.ID),
+			expected:               http.StatusConflict,
+		},
+	}
+
 	e := echo.New()
 	e.Binder = &binder.JsonApiBinder{}
 
@@ -37,16 +69,24 @@ func TestCreateBackend(t *testing.T) {
 		panic(err)
 	}
 
-	routeBackend := &BackendRoute{db: db}
-	c, recPost := postTestRequest("/v1/backends", backendPayload, e)
-	err = routeBackend.Create(c)
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusCreated, recPost.Code)
-		assert.Equal(t, binder.MIMEApplicationJSONApi, recPost.Header().Get(echo.HeaderContentType))
-		backend := &model.Backend{}
-		assert.NoError(t, jsonapi.Unmarshal(recPost.Body.Bytes(), backend))
-		assert.Equal(t, backendResult.Name, backend.Name)
-		assert.Equal(t, backendResult.ID, backend.ID)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			routeBackend := &BackendRoute{db: db}
+			c, rec := postTestRequest("/v1/backends", test.input, e)
+			err = routeBackend.Create(c)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, rec.Code)
+			if test.expectedData != nil {
+				assert.Equal(t, binder.MIMEApplicationJSONApi, rec.Header().Get(echo.HeaderContentType))
+				backend := &model.Backend{}
+				assert.NoError(t, jsonapi.Unmarshal(rec.Body.Bytes(), backend))
+				assert.Equal(t, test.expectedData.Name, backend.Name)
+				assert.Equal(t, test.expectedData.ID, backend.ID)
+			}
+			if test.expectedLocationHeader != "" {
+				assert.Equal(t, test.expectedLocationHeader, rec.Header().Get(echo.HeaderLocation))
+			}
+		})
 	}
 }
 
@@ -74,47 +114,6 @@ func TestGetBackend(t *testing.T) {
 		assert.NoError(t, jsonapi.Unmarshal(recGet.Body.Bytes(), backend))
 		assert.Equal(t, backendResult.Name, backend.Name)
 		assert.Equal(t, backendResult.ID, backend.ID)
-	}
-}
-
-func TestCreateBackendIDAlreadyExists(t *testing.T) {
-	e := echo.New()
-	e.Binder = &binder.JsonApiBinder{}
-
-	db, err := database.Database()
-	if err != nil {
-		panic(err)
-	}
-
-	routeBackend := &BackendRoute{db: db}
-	c, _ := postTestRequest("/v1/backends", backendPayload, e)
-	err = routeBackend.Create(c)
-	assert.NoError(t, err)
-
-	c, recFailPost := postTestRequest("/v1/backends", backendPayload, e)
-	err = routeBackend.Create(c)
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusConflict, recFailPost.Code)
-		assert.Equal(t, echo.MIMETextPlainCharsetUTF8, recFailPost.Header().Get(echo.HeaderContentType))
-		assert.Equal(t, recFailPost.Header().Get(echo.HeaderLocation), fmt.Sprintf("%s/v1/backends/%s", viper.GetString("serviceUrl"), backendResult.ID))
-	}
-}
-
-func TestCreateBackendNameEmpty(t *testing.T) {
-	e := echo.New()
-	e.Binder = &binder.JsonApiBinder{}
-
-	db, err := database.Database()
-	if err != nil {
-		panic(err)
-	}
-
-	routeBackend := &BackendRoute{db: db}
-	c, recPost := postTestRequest("/v1/backends", strings.Replace(backendPayload, "bind", "", -1), e)
-	err = routeBackend.Create(c)
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusBadRequest, recPost.Code)
-		assert.Equal(t, echo.MIMETextPlainCharsetUTF8, recPost.Header().Get(echo.HeaderContentType))
 	}
 }
 
