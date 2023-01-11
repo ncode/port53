10dbf87610c0ce0c2662d4af194149a4afb2011f
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-11 21:29:27 +0100

    adds extra fields and docs

--- internal/api/query.go
+++ internal/api/query.go
@@ -1,8 +1,13 @@
 package api
 
+// Query is a struct that represents a query to the API
+// https://jsonapi.org/format/#fetching
+// https://jsonapi.org/format/#fetching-sparse-fieldsets
+// https://jsonapi.org/format/#fetching-includes
+// https://jsonapi.org/format/#query-parameters
 type Query struct {
-	Filter  map[string]interface{} `query:"filter"`
-	Page    map[string]interface{} `query:"page"`
-	include string                 `query:"include"`
-	fields  map[string]string      `query:"fields"`
+	Filter  map[string]string `query:"filter"`
+	Page    map[string]int    `query:"page"`
+	include string            `query:"include"`
+	sort    string            `query:"sort"`
 }
