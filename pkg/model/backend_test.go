3b5abdad3d36fdcadbae1e33f0adbe491bc4a367
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-12 20:14:56 +0100

    adds tests for delete

--- pkg/model/backend_test.go
+++ pkg/model/backend_test.go
@@ -10,7 +10,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestGet(t *testing.T) {
+func TestBackend_Get(t *testing.T) {
 	tests := []struct {
 		name          string
 		backend       Backend
@@ -104,6 +104,123 @@ func TestGet(t *testing.T) {
 			}
 			if test.preload && len(test.backend.Zones) != len(testBackend.Zones) {
 				t.Errorf("Unexpected number of zones: got %d, want %d", len(test.backend.Zones), len(testBackend.Zones))
+			}
+		})
+	}
+}
+
+func TestBackend_BeforeCreate(t *testing.T) {
+	tests := []struct {
+		name          string
+		backend       Backend
+		expectedError error
+	}{
+		{
+			name: "Before create with empty ID",
+			backend: Backend{
+				ID: "",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Before create with valid ID",
+			backend: Backend{
+				ID: "01D4K0Z5V5J9G5J5H5R5F5E5B5",
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Before create with invalid ID",
+			backend: Backend{
+				ID: "invalid",
+			},
+			expectedError: ulid.ErrDataSize,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Call the BeforeCreate method and check the error
+			err := test.backend.BeforeCreate(nil)
+			if err != nil {
+				if err == test.expectedError {
+					return
+				}
+				t.Errorf("Unexpected error: got %s, want %s", err, test.expectedError)
+			}
+
+			// Check that the ID is set correctly
+			if test.name == "Before create with empty ID" {
+				_, err := ulid.Parse(test.backend.ID)
+				if err != nil {
+					t.Errorf("Unexpected error parsing ID: %s", err)
+				}
+			} else {
+				if test.backend.ID != "01D4K0Z5V5J9G5J5H5R5F5E5B5" {
+					t.Errorf("Unexpected ID: got %s, want %s", test.backend.ID, "01D4K0Z5V5J9G5J5H5R5F5E5B5")
+				}
+			}
+		})
+	}
+}
+
+func TestBackend_Delete(t *testing.T) {
+	tests := []struct {
+		name          string
+		backend       Backend
+		expectedError error
+	}{
+		{
+			name: "Successful delete",
+			backend: Backend{
+				ID:   ulid.Make().String(),
+				Name: ulid.Make().String(),
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Delete non-existent backend",
+			backend: Backend{
+				Name: ulid.Make().String(),
+			},
+			expectedError: nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Set up a test database and create a test backend
+			db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+			if err != nil {
+				t.Fatalf("Error setting up test database: %s", err)
+			}
+			err = db.AutoMigrate(&Backend{})
+			if err != nil {
+				t.Fatalf("Error running the migration: %s", err)
+			}
+			testBackend := Backend{
+				ID:        test.backend.ID,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				Name:      test.backend.Name,
+			}
+			if err := db.Create(&testBackend).Error; err != nil {
+				t.Fatalf("Error creating test backend: %s", err)
+			}
+
+			if test.backend.ID == "" {
+				test.backend.ID = ulid.Make().String()
+			}
+
+			// Call the Delete method and check the error
+			err = test.backend.Delete(db)
+			if err != test.expectedError {
+				t.Errorf("Unexpected error: got %s, want %s", err, test.expectedError)
+			}
+
+			// Check that the backend is deleted
+			var count int64
+			db.Model(&Backend{}).Where("id = ?", test.backend.ID).Count(&count)
+			if count != 0 {
+				t.Errorf("Backend was not deleted")
 			}
 		})
 	}
