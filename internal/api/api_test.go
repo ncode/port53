10b20fafd18192332ad3f62468edfbedbb5fccb0
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-15 19:54:46 +0100

    fix the tests for JSONAPI

--- internal/api/api_test.go
+++ internal/api/api_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/ncode/port53/pkg/binder"
+
 	"github.com/labstack/echo/v4"
 	"github.com/ncode/port53/pkg/model"
 	"github.com/stretchr/testify/assert"
@@ -20,25 +22,25 @@ func TestJSONAPI(t *testing.T) {
 			name:     "Test Case 1",
 			code:     200,
 			data:     []*model.Backend{{Name: "Backend 1", ID: "1"}, {Name: "Backend 2", ID: "2"}},
-			expected: `{"data":[{"type":"backends","id":"1","attributes":{"name":"Backend 1"}},{"type":"backends","id":"2","attributes":{"name":"Backend 2"}}]}`,
+			expected: `{"data":[{"id":"1","type":"backends","attributes":{"name":"Backend 1"},"links":{"self":"/v1/backends/1"}},{"id":"2","type":"backends","attributes":{"name":"Backend 2"},"links":{"self":"/v1/backends/2"}}]}`,
 		},
 		{
 			name:     "Test Case 2",
 			code:     201,
 			data:     &model.Backend{Name: "Backend 3", ID: "3"},
-			expected: `{"data":[{"type":"backends","id":"3","attributes":{"name":"Backend 3"}}]}`,
+			expected: `{"data":{"id":"3","type":"backends","attributes":{"name":"Backend 3"},"links":{"self":"/v1/backends/3"}}}`,
 		},
 		{
 			name:     "Test Case 3",
 			code:     404,
 			data:     nil,
-			expected: `{}`,
+			expected: `{"data":null}`,
 		},
 		{
 			name:     "Test Case 4",
 			code:     200,
 			data:     &model.Backend{ID: "01F1ZQZJXQXZJXZJXZJXZJXZJX", Name: "bind"},
-			expected: `{"data":[{"type":"backends","id":"01F1ZQZJXQXZJXZJXZJXZJXZJX","attributes":{"name":"bind"}}]}`,
+			expected: `{"data":{"id":"01F1ZQZJXQXZJXZJXZJXZJXZJX","type":"backends","attributes":{"name":"bind"},"links":{"self":"/v1/backends/01F1ZQZJXQXZJXZJXZJXZJXZJX"}}}`,
 		},
 	}
 
@@ -53,7 +55,7 @@ func TestJSONAPI(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, test.expected, rec.Body.String())
 			assert.Equal(t, test.code, c.Response().Status)
-			assert.Equal(t, []string{"application/vnd.api+json"}, c.Response().Header().Get(echo.HeaderContentType))
+			assert.Equal(t, binder.MIMEApplicationJSONApi, c.Response().Header().Get(echo.HeaderContentType))
 		})
 	}
 }
