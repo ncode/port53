a396cab361273e43272804b677bbb03d61a06667
Juliano Martinez <juliano.martinez@adyen.com>
2023-01-25 22:31:50 +0100

    adds test for delete zone

--- pkg/model/record_test.go
+++ pkg/model/record_test.go
@@ -3,6 +3,9 @@ package model
 import (
 	"testing"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	"github.com/oklog/ulid/v2"
 )
 
@@ -55,6 +58,50 @@ func TestRecode_BeforeCreate(t *testing.T) {
 				if test.record.ID != "01D4K0Z5V5J9G5J5H5R5F5E5B5" {
 					t.Errorf("Unexpected ID: got %s, want %s", test.record.ID, "01D4K0Z5V5J9G5J5H5R5F5E5B5")
 				}
+			}
+		})
+	}
+}
+
+func TestRecord_Delete(t *testing.T) {
+	tests := []struct {
+		name          string
+		record        Record
+		expectedError error
+	}{
+		{
+			name: "Delete with valid ID",
+			record: Record{
+				ID: ulid.Make().String(),
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Delete with invalid ID",
+			record: Record{
+				ID: "invalid",
+			},
+			expectedError: nil,
+		},
+	}
+	// Set up a test database and create a test zone
+	db, err := gorm.Open(sqlite.Open("file:zone?mode=memory&cache=shared"), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Error setting up test database: %s", err)
+	}
+	err = db.AutoMigrate(&Record{})
+	if err != nil {
+		t.Fatalf("Error running the migration: %s", err)
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Call the Delete method and check the error
+			err := test.record.Delete(db)
+			if err != nil {
+				if err == test.expectedError {
+					return
+				}
+				t.Errorf("Unexpected error: got %s, want %s", err, test.expectedError)
 			}
 		})
 	}
